a75403832ca838507b62ce2c21bcc85942f43cb2
Shivkumardhakad <shivkumardhakad9981@gmail.com>
2025-07-12 12:54:03 +0530

    ADDED ITEM PAGE

--- routes/home.js
+++ routes/home.js
@@ -50,6 +50,17 @@ router.post("/items/new", isLoggedin, upload.array("images", 5), async (req, res
   }
 });
 
+router.get("/items/:id", async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id).populate("uploader");
+    if (!item) return res.status(404).send("Item not found");
+
+    res.render("homes/eachItem", { item, title: item.title });
+  } catch (err) {
+    console.error(err);
+    res.send("Error loading item");
+  }
+});
 
 
 module.exports = router;
